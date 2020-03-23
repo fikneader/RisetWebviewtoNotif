344698e8560f5974a815cf7f0ecb171629d8f8c2
fikneader <fikneader@gmail.com>
2020-03-23 16:39:45 +0700

    23 Maret 2020

--- app/src/main/java/online/botika/risetwebviewtonotif/AboutActivity.java
+++ app/src/main/java/online/botika/risetwebviewtonotif/AboutActivity.java
@@ -1,26 +1,149 @@
 package online.botika.risetwebviewtonotif;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class AboutActivity extends AppCompatActivity {
+
+    FileOutputStream outStream = null;
+    public static final int RequestPermissionCode = 1;
+    private String TAG = "Botika";
+    Bitmap bitmap;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        final Button next = findViewById(R.id.button_next);
-        next.setOnClickListener(new View.OnClickListener() {
+        Button download = findViewById(R.id.button_download);
+        imageView = findViewById(R.id.imageView);
+
+        download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                FileOutputStream fileOutputStream=null;
+                File file=getdisc();
+                if (!file.exists() && !file.mkdirs())
+                {
+                    Toast.makeText(getApplicationContext(),"sorry can not make dir",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyymmsshhmmss");
+                String date=simpleDateFormat.format(new Date());
+                String name="img"+date+".jpeg";
+                String file_name=file.getAbsolutePath()+"/"+name; File new_file=new File(file_name);
+                try {
+                    fileOutputStream =new FileOutputStream(new_file);
+                    Bitmap bitmap=viewToBitmap(imageView,imageView.getWidth(),imageView.getHeight());
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100,fileOutputStream);
+                    Toast.makeText(getApplicationContext(),"sucses", Toast.LENGTH_LONG).show();
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                }
+                catch
+                (FileNotFoundException e) {
+
+                } catch (IOException e) {
+
+                } refreshGallary(file);
+            } private void refreshGallary(File file)
+            { Intent i=new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                i.setData(Uri.fromFile(file)); sendBroadcast(i);
+
+            }
+            private File getdisc(){
+                File file= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+                return new File(file,"My Image");
             }
         });
+    }
+
+    private static Bitmap viewToBitmap(View view, int widh, int hight)
+    {
+        Bitmap bitmap=Bitmap.createBitmap(widh,hight, Bitmap.Config.ARGB_8888);
+        Canvas canvas=new Canvas(bitmap); view.draw(canvas);
+        return bitmap;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(AboutActivity.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, RequestPermissionCode);
+    }
+
+    @SuppressLint({"MissingPermission", "HardwareIds"})
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length> 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(AboutActivity.this, "Ijin diberikan",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(AboutActivity.this,"Ijin Ditolak", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED ;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (checkPermission()){
+            Log.i(TAG, "Permission Granted");
+        }
+        else {
+            requestPermission();
+        }
     }
 }
--- app/src/main/java/online/botika/risetwebviewtonotif/MainActivity.java
+++ app/src/main/java/online/botika/risetwebviewtonotif/MainActivity.java
@@ -3,53 +3,177 @@ package online.botika.risetwebviewtonotif;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import online.botika.risetwebviewtonotif.service.BackgroundService;
+import online.botika.risetwebviewtonotif.service.SharedPreference;
+import online.botika.risetwebviewtonotif.service.StatusService;
 
 import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     String TAG = "Botika";
+    private String urlku,authku;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getAuth();
         initComponent();
+
+        //creating a new user object
+        StatusService statusService = new StatusService("STARTSERVICE");
+        //storing the user in shared preferences
+        SharedPreference.getInstance(getApplicationContext()).statusService(statusService);
+
+        //start
+        Intent startIntent = new Intent(MainActivity.this, BackgroundService.class);
+        startIntent.setAction("STARTSERVICE");
+        startService(startIntent);
+
+    }
+
+    public void initComponent(){
+        webView = findViewById(R.id.webview);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    public void getAuth(){
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        final JSONObject jsonBodyObj = new JSONObject();
+        String url = "https://client.botmaster2.botika.online/console-botika/authentication";
+        try{
+            jsonBodyObj.put("username", "andripgbyk");
+            jsonBodyObj.put("password", "082136523735");
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        final String requestBody = jsonBodyObj.toString();
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                url, null, new Response.Listener<JSONObject>(){
+            @Override    public void onResponse(JSONObject response) {
+                try {
+                    //converting response to json object
+                    JSONObject obj = new JSONObject(response.toString());
+                    Log.i(TAG,obj.getString("result"));
+                    //if no error in response
+                    String result = obj.getString("result");
+                    if (result.equals("error")){
+                        Toast.makeText(getApplicationContext(),obj.getString("message"),Toast.LENGTH_SHORT).show();
+                    } else {
+                        progressBar.setVisibility(View.GONE);
+                        loadWebview(obj.getString("auth"));
+//                        loadWebview("");
+//                        Toast.makeText(getApplicationContext(),obj.getString("auth"),Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override    public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override    public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+
+            @Override    public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+
+
+        };
+
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(0, -1, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(jsonObjectRequest);
     }
 
     @SuppressLint("SetJavaScriptEnabled")
-    public void initComponent(){
-        webView = findViewById(R.id.webview);
+    public void loadWebview(final String auth){
+//        urlku = "https://client.botmaster2.botika.online/console-botika/?auth="+auth;
+        urlku = "http://ngeartstudio.com/";
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
         webView.getSettings().setAllowFileAccessFromFileURLs(true);
         webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
         webView.getSettings().setSupportMultipleWindows(true);
         webView.getSettings().setPluginState(WebSettings.PluginState.ON);
-//        webView.setWebChromeClient(new WebChromeClient());
-        webView.loadUrl("http://ngeartstudio.com/");
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl(urlku);
+
+        webView.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK
+                        && event.getAction() == MotionEvent.ACTION_UP
+                        && webView.canGoBack()) {
+                    webView.goBack();
+                    return true;
+                }
+                return false;
+            }
+
+        });
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if ("http://ngeartstudio.com/".equals(Uri.parse(url).getHost())) {
+                if (urlku.equals(Uri.parse(url).getHost())) {
                     // This is my website, so do not override; let my WebView load the page
                     return false;
                 }
@@ -74,11 +198,17 @@ public class MainActivity extends AppCompatActivity {
 
         /** Show a notif from the web page */
         @JavascriptInterface
-        public void showAndroidNotif() {
+        public void showAndroidNotif(String value) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                showNotif();
+                showNotif(value);
             }
         }
+
+//        public void showAndroidNotif() {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                showNotif("Tittle");
+//            }
+//        }
 
         /** Show a notif from the web page */
         @JavascriptInterface
@@ -94,13 +224,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public  void showNotif(){
+    public  void showNotif(String value){
         Intent intent=new Intent(getApplicationContext(),MainActivity.class);
         String CHANNEL_ID="MYCHANNEL";
         NotificationChannel notificationChannel=new NotificationChannel(CHANNEL_ID,"name", NotificationManager.IMPORTANCE_LOW);
         PendingIntent pendingIntent=PendingIntent.getActivity(getApplicationContext(),1,intent,0);
         Notification notification=new Notification.Builder(getApplicationContext(),CHANNEL_ID)
-                .setContentText("Heading")
+                .setContentText(value)
                 .setContentTitle("subheading")
                 .setContentIntent(pendingIntent)
                 .addAction(android.R.drawable.sym_action_chat,"Title",pendingIntent)
@@ -111,5 +241,10 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.createNotificationChannel(notificationChannel);
         notificationManager.notify(1,notification);
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
     }
 }
